5adff32b0444d2f371cd9c4df479cb911a338bc7
Duncan MacKenzie <duncan.mackenzie@phe.gov.uk>
2016-01-31 11:14:44 +0000

    Remove boilerplate text in About and Home

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,50 +1,13 @@
 import React, {Component} from 'react';
 import Helmet from 'react-helmet';
-import { MiniInfoBar } from 'components';
 
 export default class About extends Component {
+  render = () =>
+    <div className="container">
+      <h1>About Us</h1>
+      <Helmet title="About Us"/>
 
-  state = {
-    showKitten: false
+      <p>This project was conceived and directed by Dr Rajiv Chandegra and made by Harmeet ____, Steve O'Connell, Claude Griffiths, Chris Emerson and Duncan MacKenzie at <a href="http://www.nhshackday.com/">NHS Hack Day 2016</a>.
+      </p>
+    </div>
   }
-
-  handleToggleKitten = () => this.setState({showKitten: !this.state.showKitten});
-
-  render() {
-    const {showKitten} = this.state;
-    const kitten = require('./kitten.jpg');
-    return (
-      <div className="container">
-        <h1>About Us</h1>
-        <Helmet title="About Us"/>
-
-        <p>This project was originally created by Erik Rasmussen
-          (<a href="https://twitter.com/erikras" target="_blank">@erikras</a>), but has since seen many contributions
-          from the open source community. Thank you to <a
-            href="https://github.com/erikras/react-redux-universal-hot-example/graphs/contributors"
-            target="_blank">all the contributors</a>.
-        </p>
-
-        <h3>Mini Bar <span style={{color: '#aaa'}}>(not that kind)</span></h3>
-
-        <p>Hey! You found the mini info bar! The following component is display-only. Note that it shows the same
-          time as the info bar.</p>
-
-        <MiniInfoBar/>
-
-        <h3>Images</h3>
-
-        <p>
-          Psst! Would you like to see a kitten?
-
-          <button className={'btn btn-' + (showKitten ? 'danger' : 'success')}
-                  style={{marginLeft: 50}}
-                  onClick={this.handleToggleKitten}>
-            {showKitten ? 'No! Take it away!' : 'Yes! Please!'}</button>
-        </p>
-
-        {showKitten && <div><img src={kitten}/></div>}
-      </div>
-    );
-  }
-}
